9376fb6c7c190f401a9876cb722e292bc6ad6fed
fkuca <fkuca91@gmail.com>
2024-06-15 17:08:58 +0200

    chore: add Prettier, ESLint, SASS with configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,25 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "eslint-config-prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    eqeqeq: ["error"],
+    "max-depth": ["error"],
+    "max-params": ["error"],
+    "no-console": ["error"],
+    "no-magic-numbers": ["error"],
+    "sort-imports": ["error"],
   },
-}
+};
+
